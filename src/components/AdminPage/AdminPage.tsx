a557974acf79832e3daf17713b9c742753644ef5
Varga Albert <albi1000@icloud.com>
2023-11-20 11:50:27 +0100

    admin page style

--- src/components/AdminPage/AdminPage.tsx
+++ src/components/AdminPage/AdminPage.tsx
@@ -7,7 +7,6 @@ import UserHitResult from "../HitResult/HitResult";
 
 const AdminPage: React.FC = () => {
 
-
     const id = "admin"
 
     const {
@@ -21,6 +20,7 @@ const AdminPage: React.FC = () => {
         adminGenerateTicket,
         startGenerateAdminLotteryTicket,
         resetGame,
+        formatPrice
     } = useLottoContext();
 
     return (
@@ -30,60 +30,76 @@ const AdminPage: React.FC = () => {
                 <div className={style.controlContainer}>
 
                     <div className={style.profileContainer}>
-
                         <div className={style.iconContainer}>
-                            <FaUser className={style.icon} />
-                            <input type="text"
-                                onChange={(e) => { handleInputChange(e, id) }}
-                                onBlur={(e) => { handleBlurChange(e, id) }}
-                                placeholder="irj egy nevet."
-                                value={setValue(id)}>
-                            </input>
+                            <div className={style.iconCont}>
+                                <FaUser className={style.icon} />
+                                admin
+                            </div>
+                            <div className={style.nameInputCont}>
+                                <label htmlFor="customName">
+                                    Felhasználónév:
+                                </label>
+                                <input
+                                    type="text"
+                                    id="customName"
+                                    onChange={(e) => { handleInputChange(e, id) }}
+                                    onBlur={(e) => { handleBlurChange(e, id) }}
+                                    placeholder="Irj egy nevet."
+                                    value={setValue(id)}>
+                                </input>
+                            </div>
                         </div>
-
                         <p>Account balance</p>
                         <div className={style.balanceMainContainer}>
-
                             <div className={style.balanceContainer}>
-                                <div className={style.balance}>{getMoney(id)}</div>
-                                <div className={style.akcse}>Akcse</div>
+                                <div className={style.title}>Admin akcse:</div>
+                                <div className={style.balance}>
+                                    {formatPrice(getMoney(id))} Akcse
+                                </div>
                             </div>
                             <div className={style.balanceContainer}>
-                                <div className={style.balance}>{getMoney("profit")}</div>
-                                <div className={style.akcse}>profit</div>
+                                <div className={style.title}>Lotto profit:</div>
+                                <div className={style.balance}>
+                                    {formatPrice(getMoney("profit"))} Akcse
+                                </div>
                             </div>
                             <div className={style.balanceContainer}>
-                                <div className={style.balance}>{getMoney("collector")}</div>
-                                <div className={style.akcse}>gyüjtö</div>
+                                <div className={style.title}>Nyeremény alap:</div>
+                                <div className={style.balance}>
+                                    {formatPrice(getMoney("collector"))} Akcse
+                                </div>
                             </div>
-
                         </div>
-
                     </div>
 
                     <div className={style.adminControlContainer}>
-
-                        <div>
-                            <label htmlFor="generet-num">Generált szelvények:</label>
-                            <input id="generet-num"
+                        <div className={style.generateTicketContainer}>
+                            <label htmlFor="generet-num">Szelvény generálás db:</label>
+                            <input
+                                id="generet-num"
+                                className={style.generateTicketInput}
                                 type="number"
                                 onChange={setGenerateTicket}
                                 value={adminGenerateTicket} />
-                            <div>
-                                <FaArrowRotateLeft onClick={() => setAdminGenerateTicket("")} />
+                            <div className={style.startGenerateCont}>
                                 <button onClick={startGenerateAdminLotteryTicket} > generálás</button>
+                                <FaArrowRotateLeft
+                                    className={style.icon}
+                                    onClick={() => setAdminGenerateTicket("")} />
                             </div>
                         </div>
-
-
-                        <button onClick={() => { startLottery(); }}>Sorsolás inditása</button>
-
-                        <button onClick={() => { resetGame(); }}>Játék újrainditása.</button>
-
+                        <div className={style.btnContainer}>
+                            <button
+                                onClick={() => { startLottery(); }}>Sorsolás inditása
+                            </button>
+                            <button
+                                className={style.reset}
+                                onClick={() => { resetGame(); }}>Játék újrainditása.
+                            </button>
+                        </div>
                     </div>
 
                     <LotteryTicketList id={""} />
-
                 </div>
 
                 <UserHitResult id={""} />
